76a55200c2e2f246c95fd545c0ab5c5dbe272661
Kong To <newlight77@gmail.com>
2019-08-16 18:06:29 +0200

    making the top nav bar sticky

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -38,6 +38,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to oneprofile');
+    expect(compiled.querySelector('h1').textContent).toContain('OneProfile');
   });
 });
